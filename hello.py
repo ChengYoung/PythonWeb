cef6f2680e98f5096929d4783b2fc7bba482fd34
qianc <qianc@cig.com.cn>
2018-03-21 02:14:08 -0700

    march 21

--- hello.py
+++ hello.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/user/<name>')
-def index(name):
-    return '<h1>hello %s !</h1>' %name
+def user(name):
+    return render_template('user.html',name=name)
 
+@app.errorhandler(404)
+def page_no_found():
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error():
+    return render_template('500.html'),500:
 
 if __name__ =="__main__":
-    app.run(host='0.0.0.0',debug = True)
+    manager.run()
