4614c05ad385da2c76e3497c9ce8efc9193ce42d
qianc <qianc@cig.com.cn>
2018-03-22 03:29:47 -0700

    complete form

--- hello.py
+++ hello.py
@@ -3,15 +3,28 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name=StringField('What is your name ?',validators=[DataRequired()])
+    submit=SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+    return render_template('index.html',form=form,name=name,current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
