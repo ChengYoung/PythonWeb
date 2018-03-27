4c9075267a52558e7a2790394ca6a05c274ac05f
mohan <774841525@qq.com>
2018-03-27 18:32:40 +0800

    start

--- hello.py
+++ hello.py
@@ -15,7 +15,6 @@ class NameForm(FlaskForm):
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=\'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-#db=SQLALchemy(app)
 db = SQLALchemy(app)
 app.config['SECRET_KEY']='hard to guess string'
 manager = Manager(app)
