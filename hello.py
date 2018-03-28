50ec2b2a10b7594c637bc6400ccf0a9f150cb90a
钱程 <qianc@cig.com.cn>
2018-03-28 18:20:30 +0800

    'march28'

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -12,14 +13,33 @@ class NameForm(FlaskForm):
     name=StringField('What is your name ?',validators=[DataRequired()])
     submit=SubmitField('Submit')
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=\'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-db = SQLALchemy(app)
 app.config['SECRET_KEY']='hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI']=\
+    'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='role')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/',methods=['GET','POST'])
 def index():
