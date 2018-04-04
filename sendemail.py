da67eb7530363ad7be32d6dfbf09e01ff81cd742
钱程 <qianc@cig.com.cn>
2018-04-04 17:20:28 +0800

    '6atest'

--- sendemail.py
+++ sendemail.py
@@ -2,7 +2,11 @@ import os
 from flask import Flask
 from flask_mail import Mail
 
-
-
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.163.com'
+app.config['MAIL_PORT']=25
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWARD']=os.environ.get('MAIL_PASSWARD')
+
 mail = Mail(app)
