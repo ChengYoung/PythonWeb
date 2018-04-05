e178f528639380ba8a6a0d9e32a060c23b4d189a
mohan <774841525@qq.com>
2018-04-05 16:57:43 +0800

    6a middle

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_migrate import Migrate,MigrateCommand
+from flask_mail import Mail
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -20,11 +21,18 @@ app.config['SECRET_KEY']='hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI']=\
     'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+app.config['MAIL_SERVER']='smtp.163.com'
+app.config['MAIL_PORT']=25
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
+
 
 db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
--- sendemail.py
+++ sendemail.py
@@ -1,13 +1,19 @@
 import os
 from flask import Flask
 from flask_mail import Mail
+from flask_script import Manager
 
-#configuration
 app = Flask(__name__)
 app.config['MAIL_SERVER']='smtp.163.com'
-app.config['MAIL_PORT']=25
-app.config['MAIL_USE_TLS']=True
+app.config['MAIL_PORT']=465
+#app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USE_SSL']=True
 app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWARD']=os.environ.get('MAIL_PASSWARD')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
 
 mail = Mail(app)
+manager = Manager(app)
+
+
+if __name__=='__main__':
+    manager.run()
