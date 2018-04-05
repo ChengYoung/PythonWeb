044f07fdbdc97f82edeb42450afa93d5922effb3
钱程 <201722210009@example.com>
2018-04-05 21:06:38 +0800

    apirl5

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_migrate import Migrate,MigrateCommand
-from flask_mail import Mail
+from flask_mail import Mail,Message
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -21,12 +21,14 @@ app.config['SECRET_KEY']='hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI']=\
     'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+
 app.config['MAIL_SERVER']='smtp.163.com'
-app.config['MAIL_PORT']=25
-app.config['MAIL_USE_TLS']=True
+app.config['MAIL_PORT']=465
+app.config['MAIL_USE_SSL']=True
 app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
-
+app.config['FLASKY_MAIL_SUBJECT_PREFIX']='[Flasky]'
+app.config['FLASKY_MAIL_SENDER']='Flasky Admin <>'
 
 db = SQLAlchemy(app)
 manager = Manager(app)
@@ -68,6 +70,14 @@ def index():
         form.name.data=''
         return redirect(url_for('index'))
     return render_template('index.html',form=form,name=session.get('name'),known = session.get('known',False),current_time=datetime.utcnow())
+
+def send_email(to,subject,template,**kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    msg.body = render_template(template+'.txt',**kwargs)
+    msg.html = render_template(template+'.html',**kwargs)
+    mail.send(msg)
+
 
 @app.route('/user/<name>')
 def user(name):
